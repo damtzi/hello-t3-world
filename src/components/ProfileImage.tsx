47126148b96bc29a17c8d4c6ecbc1a674ce19871
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2023-05-29 23:14:26 +0100

    Complete tutorial

---
+++ src/components/ProfileImage.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import React from 'react';
+import { VscAccount } from 'react-icons/vsc';
+
+type ProfileImageProps = {
+    src?: string | null;
+    className?: string;
+};
+
+const ProfileImage: React.FC<ProfileImageProps> = ({ src, className = "" }) => {
+    return <div className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}>
+        {src == null ? <VscAccount className="h-full w-full" /> : <Image src={src} alt="PRofile Image" quality={100} fill />}
+    </div>;
+}
+
+export default ProfileImage
